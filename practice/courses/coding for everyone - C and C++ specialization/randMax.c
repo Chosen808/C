222ac244420151855457451c1a3b58f323c04d5e
Chosen808 <learnkingdom@gmail.com>
2023-04-10 22:20:43 -0400

    Commit 3 - 10423 - 2220

---
+++ practice/courses/coding for everyone - C and C++ specialization/randMax.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
+
+main ()
+{
+    int a, above_cnt = 0, below_cnt = 0, diff, total;
+    double median = RAND_MAX/2.0;
+    double avg;
+
+    long start, finish, diff2, min, sec;
+
+    total = 0;
+
+    printf ("%10s%15s%10s%10s%10s%15s%15s%10s \n", "count", "rand", "above_cnt", "below_cnt", "diff", "total", "average", "timer");
+    printf ("%10s%15s%10s%10s%10s%15s%15s%10s \n", "_____", "__________", "_____", "_____", "_____", "_____", "__________", "_____");
+
+    start = time (NULL);
+
+    for (int i = 1; i < 10000001; i++)
+    {
+        a = rand ();
+
+        if ( a > median)
+        {
+            above_cnt++;
+        }
+
+        else if ( a < median)
+        {
+            below_cnt++;
+        }
+
+        diff = above_cnt - below_cnt;
+        total+= diff;
+        avg = total / 10000000.00;
+
+        finish = time (NULL);
+
+        diff2 = finish - start;
+
+        min = diff2 / 60;
+        sec = diff2 % 60;
+
+        printf ("%10d%15d%10d%10d%10d%15d%15lf%10ld:%ld \n", i, a, above_cnt, below_cnt, diff, total, avg, min, sec);
+    }
+
+    //printf ("total = %d \naverage = %lf \n", total, avg);
+
+    return 0;
+}
--- practice/courses/coding for everyone - C and C++ specialization/time.c
+++ practice/courses/coding for everyone - C and C++ specialization/time.c
@@ -3,23 +3,35 @@
 #include <time.h>
 
 #define NCALLS 999999999 // number of fct calls
-#define NCOLS 20 // number of columns
+#define NCOLS 99999 // number of columns
 #define NLINES 3    // number of line 
 
 main (void)
 {
-    int i, val;
+    int i, val, a;
     //double val;
     long begin, diff, end;
 
+    clock_t start, finish;
+    double result;
+
     begin = time (NULL);
-    srand (time (NULL));
+    srand (rand());
+
+    start = clock ();
+
+    printf ("begin = %ld \nsrand = %d \n", begin, srand );
+
 
     printf ("\n TIMING TEST: %d calls to rand() \n\n", NCALLS);
 
     for ( i = 1; i <= NCOLS; ++i)
     {
         val = rand ();
+
+        printf ("NCOLS * NLINES = %d \n", NCOLS * NLINES);
+
+        printf ("i = %d \n", i);
 
         if ( i <= NCOLS * NLINES)
         {
@@ -39,6 +51,13 @@ main (void)
 
     end = time (NULL);
     diff = end - begin;
+
+    finish = clock ();
+
+    result = (double) finish - start;
+    result = result / CLOCKS_PER_SEC;
+
+    printf ("start = %ld \nfinish = %ld \ntimer = %lf \n", start, finish, result);
 
     printf ("%s%ld \n%s%ld\n%s%ld\n%s%.10f \n\n", "end time: ", end, "begin time: ", begin, "elapsed time: ", diff, "time for each call: ", (double) diff/NCALLS);
 
