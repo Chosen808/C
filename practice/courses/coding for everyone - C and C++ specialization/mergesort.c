335aaf0b06135d60e960eb81e472c481cb2c3d87
Chosen808 <learnkingdom@gmail.com>
2023-04-19 22:54:05 -0400

    Commit 3 - 19423 - 2252

--- practice/courses/coding for everyone - C and C++ specialization/mergesort.c
+++ practice/courses/coding for everyone - C and C++ specialization/mergesort.c
@@ -1,35 +1,62 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
 
+void print_array (int how_many, int data[], char *str)
+{
+    printf ("%s", str);
 
+    for (int i = 0; i < how_many; i++)
+    {
+        printf ("%d\t", data[i]);
+    }
+}
 
-
-
-
-
-
-void merge ( int a[], int b[], int c[], int m, int n)
+void merge (int a[], int b[], int c[], int how_many)
 {
     int i = 0, j = 0, k = 0;
 
-    while ( i < m && k < n)
+    while ( i < how_many && j < j < how_many)
     {
-        if ( a[i] < b[j])
+        if (a[i] < b[j])
         {
-            c[k++] = a[a++];
+            c[k++] = a[i++];
         }
 
-        else
+        else 
+        {
+            c[k++] = b[j++];
+        }
+
+        while ( i < how_many )
+        {
+            c[k++] = a[i++];
+        }
+
+        while ( j < how_many)
         {
             c[k++] = b[j++];
         }
     }
+}
 
-    while ( i < m)
-    {
-        c[k++] = a[i++];
-    }
+main (void)
+{
+    const int size = 5;
+    int a[size] = {99, 67, 88, 82, 83};
+    int b[size] = {88, 58, 81,69, 72};
+    int c[2 * size];
 
-    while (j < n)
-    {
-        c[k++] = b[j++];
-    }
+    print_array (size, a, "My grades\n");
+    puts ("\n\n");
+
+    print_array (size, b, "More grades\n");
+    puts("\n\n");
+
+    merge (a, b, c, size);
+
+    print_array ( 2 * size, c, "My sorted grades\n");
+    puts ("\n\n");
+
+    return 0;
 }
