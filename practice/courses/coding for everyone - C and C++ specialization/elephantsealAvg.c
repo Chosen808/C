eb089827aa2784a12753500316ec1f2e0434b099
Chosen808 <learnkingdom@gmail.com>
2023-04-22 16:53:20 -0400

    Commit 2 - 22423 - 1653

--- practice/courses/coding for everyone - C and C++ specialization/elephantsealAvg.c
+++ practice/courses/coding for everyone - C and C++ specialization/elephantsealAvg.c
@@ -8,7 +8,7 @@ void print_array (int how_many, int data[], char *str)
 
     for ( int i = 0; i < how_many; i++)
     {
-        printf ("%d\t", data[i]);
+        printf ("%d\n", data[i]);
     }
 }
 
@@ -40,11 +40,11 @@ void merge ( int a[], int b[], int c[], int how_many)
     }
 }
 
-void fileread ( char *filename )
+int fileread ( char *filename, int size, int data[])
 {
-    int j = 1, size = 0, temp = 0;
-    int numbers[100];
-    int data[1000000];
+    int j = 1, temp = 0;
+    char numbers[100];
+    char nums[100];
     
     FILE *popen = fopen ( filename, "r");
 
@@ -52,37 +52,54 @@ void fileread ( char *filename )
     {
         puts ("No such file exist\n");
 
-        //return fileread;
     }
 
-    while ( fscanf ( popen, "%s", numbers) == 1)
+    while ( fscanf ( popen, "%s", nums) == 1)
     {
-        j++;
-        temp = atoi (numbers);
+        size = j++;
+        data[size];
+        temp = atoi (nums);
         data[j] = temp;
     }
 
     for ( int i = 1; i < j; i++)
     {
-        printf ("Data ID%d: %d\n", i, data[i]);
+       printf ("Data ID%d: %d\n", i, data[i]);
     }
 
+    return data;
 }
 
 void mergesort ( int key[], int how_many)
 {
     int j, k;
 
+    int w[how_many];
+
     for ( k = 1; k < how_many; k++)
     {
+        for ( j = 0; j < how_many - k; j += 2*k)
+        {
+            merge ( key + j, key + j + k, w + j, k);
+        }
 
+        for ( j = 0; j < how_many; j++)
+        {
+            key[j] = w[j];
+        }
     }
 }
 
 int main ()
 {
+    int size;
+    int data[0];
 
-    fileread ("ele_seal.txt");
+    fileread ("elephant_seal.txt", size, data);
+
+    mergesort (data, size);
+
+    print_array (size, data, "My sorted data\n");
 
     return 0;
 }
