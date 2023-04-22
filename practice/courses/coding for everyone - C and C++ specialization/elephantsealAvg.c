4c907d9d8d73a1505f6383cad44c507586a4913f
Chosen808 <learnkingdom@gmail.com>
2023-04-22 14:54:22 -0400

    Commit 1 - 22423 - 1454

--- practice/courses/coding for everyone - C and C++ specialization/elephantsealAvg.c
+++ practice/courses/coding for everyone - C and C++ specialization/elephantsealAvg.c
@@ -40,17 +40,13 @@ void merge ( int a[], int b[], int c[], int how_many)
     }
 }
 
-void fileread ( void)
+void fileread ( char *filename )
 {
-    char filename[100];
-    int j = 0, size = 0, temp;
+    int j = 1, size = 0, temp = 0;
     int numbers[100];
-    int data[10000000];
-
-    printf ("j = %d\n", j);
-
+    int data[1000000];
     
-    FILE *popen = fopen ( "ele_seal.txt", "r");
+    FILE *popen = fopen ( filename, "r");
 
     if ( popen == NULL)
     {
@@ -59,15 +55,14 @@ void fileread ( void)
         //return fileread;
     }
 
-    while ( fscanf ( popen, "%*s", numbers) == 1)
+    while ( fscanf ( popen, "%s", numbers) == 1)
     {
         j++;
-        printf ("j = %d\n", j);
         temp = atoi (numbers);
         data[j] = temp;
     }
 
-    for ( int i = 0; i < j; i++)
+    for ( int i = 1; i < j; i++)
     {
         printf ("Data ID%d: %d\n", i, data[i]);
     }
@@ -86,45 +81,8 @@ void mergesort ( int key[], int how_many)
 
 int main ()
 {
-    puts ("TEST\n");
 
-    char filename[100] = "ele_seal.txt";
-    int j = 0, size = 0, temp;
-    int numbers[100];
-    int data[10000000];
+    fileread ("ele_seal.txt");
 
-    j+=1;
-
-    printf ("Test\n");
-
-    
-    FILE *popen = fopen ( "ele_seal.txt", "r");
-
-    if ( popen == NULL)
-    {
-        puts ("No such file exist\n");
-
-        //return fileread;
-    }
-
-    else 
-    {
-        while ( fscanf ( popen, "%*s", numbers) == 1)
-        {
-            j++;
-            printf ("j = %d\n", j);
-            temp = atoi (numbers);
-            data[j] = temp;
-        }
-
-        for ( int i = 0; i < j; i++)
-        {
-            printf ("Data ID%d: %d\n", i, data[i]);
-        }
-
-        puts ("Test\n");
-
-        //fileread ();
-    }
     return 0;
 }
