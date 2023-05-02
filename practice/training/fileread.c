032209aa6853d0f4f78340c25074da073459e2fd
Chosen808 <learnkingdom@gmail.com>
2023-05-01 23:48:50 -0400

    Commit 2 - 5123 - 2348

---
+++ practice/training/fileread.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "merge.h"
+
+main ()
+{
+    int size = 1001;
+    int arr[1001] = {};
+
+    fileread (arr, size);
+
+    puts ("\n\n");
+
+    arr_print (arr, size, "My Unsorted Data", "ID", "Data");
+
+    mergesort (arr, 0, size);
+
+    puts ("\n\n");
+
+    arr_print (arr, size, "My Sorted Data", "ID", "Data");
+
+    return 0;
+}
--- practice/training/merge.h
+++ practice/training/merge.h
@@ -3,7 +3,7 @@
 
 int fileread ( int data[], int size)
 {
-    char *filename;
+    char filename[100];
     int i = 0, temp = 0;
     char numbers[100];
 
@@ -30,3 +30,82 @@ int fileread ( int data[], int size)
 
 }
 
+void arr_print (int data[], int size, char *str1, char *str2, char *str3)
+{
+    printf ("%s \n", str1);
+    printf ("%10s%10s \n", str2, str3);
+    printf ("%10s%10s \n", "_____", "_____");
+
+    for (int i = 0; i < size; i++)
+    {
+        printf ("%10d%10d \n", i, data[i]);
+    }
+}
+
+void merge (int data[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+    {
+        L[i] = data[l + i];
+    }
+
+    for (j = 0; j < n2; j++)
+    {
+        R[j] = data[m + 1 + j];
+    }
+
+    i = 0;
+    j = 0;
+    k = l;
+
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            data[k] = L[i];
+            i++;
+        }
+
+        else
+        {
+            data[k] = R[j];
+            j++;
+        }
+
+        k++;
+    }
+
+    while (i < n1)
+    {
+        data[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (i < n2)
+    {
+        data[k] = R[j];
+        j++;
+        k++;
+    }
+
+}
+
+void mergesort (int data[], int l, int r)
+{
+    if (l < r)
+    {
+        int m = l + (r - l) / 2;
+
+        mergesort (data, l, m);
+        mergesort (data, m + 1, r);
+
+        merge (data, l, m, r);
+    }
+}
