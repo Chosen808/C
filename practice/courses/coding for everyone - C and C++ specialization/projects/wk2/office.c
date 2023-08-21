12fe618cada533561a496cfd3ec4ad205413042a
Chosen808 <learnkingdom@gmail.com>
2023-08-21 15:46:32 -0400

    Commit 21823 - 1

--- practice/courses/coding for everyone - C and C++ specialization/projects/wk2/office.c
+++ practice/courses/coding for everyone - C and C++ specialization/projects/wk2/office.c
@@ -1,14 +1,50 @@
 #include <stdio.h>
 
-struct hr
-{
-
-};
-
-enum
+// These are the departments
+typedef enum departments
 {
     sales,
     research,
     software,
     executive
+} humanres;
+
+// This is struct that holds the business data
+struct llc
+{
+    humanres HR;
+
+    int annSal;
+    unsigned ssn;
 };
+
+struct llc salary;
+
+int func ()
+{
+    
+}
+
+int salGen ()
+{
+    struct llc depart;
+
+    depart.HR = sales;
+
+    printf ("SALES = %d \n", depart.HR);
+
+    depart.HR = research;
+
+    printf ("RESEARCH = %d \n", depart.HR);
+
+    return 0;
+
+}
+
+
+main ()
+{
+    salGen ();
+
+    return 0;
+}
