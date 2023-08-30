12cb26044ae1c5737e656bddae0992107cf2fa4a
Chosen808 <learnkingdom@gmail.com>
2023-08-30 16:18:29 -0400

    Commit - 30823 - 1618 - 2

--- practice/courses/coding for everyone - C and C++ specialization/projects/wk3/for.c
+++ practice/courses/coding for everyone - C and C++ specialization/projects/wk3/for.c
@@ -7,9 +7,9 @@ main ()
 
     for (i = 0; i < 10; i++)
     {
-        for (j = -1; j < 10; i++)
+        for (j = -1; j < 10; j++)
         {
-            var = 20 * 10;
+            var = 4 * 2;
             temp += var;
 
             printf ("%d \n", temp);
