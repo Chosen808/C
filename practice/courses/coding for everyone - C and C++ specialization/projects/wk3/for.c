b788764ea9e3e8660bce2fb6c5a78fe8f7a74500
Chosen808 <learnkingdom@gmail.com>
2023-08-30 16:08:31 -0400

    Commit - 30823 - 1608 - 1

---
+++ practice/courses/coding for everyone - C and C++ specialization/projects/wk3/for.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main ()
+{
+    int i = 0, j = 0, var = 0, temp = 0;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (j = -1; j < 10; i++)
+        {
+            var = 20 * 10;
+            temp += var;
+
+            printf ("%d \n", temp);
+        }
+    }
+
+    return 0;
+}
