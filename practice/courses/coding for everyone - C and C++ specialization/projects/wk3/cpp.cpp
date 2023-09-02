e31747c44e9b6065d96153c3bef6a07ab636e9bd
Chosen808 <learnkingdom@gmail.com>
2023-09-02 18:04:50 -0400

    Commit - 2923 - 1804 - 4

---
+++ practice/courses/coding for everyone - C and C++ specialization/projects/wk3/cpp.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+main ()
+{
+    long a = 0, b = 0, c = 0;
+
+    printf ("Enter a number: ");
+    scanf ("%d", &a);
+
+    for (int i = 0; i < 5; i++)
+    {
+        b += a * i;
+        c += b * b * i;
+        a += a * b * c * i;
+
+        printf ("Answer b = %ld \n", b);
+        printf ("Answer c = %ld \n", c);
+        printf ("Answer a = %ld \n", a);
+    }
+
+    return 0;
+}
