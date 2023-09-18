140c3162a4e77c197cf78301154a3317baf6ccf2
Chosen808 <learnkingdom@gmail.com>
2023-09-18 15:45:51 -0400

    Commit - 18923 - 1545 - 3

--- practice/courses/coding for everyone - C and C++ specialization/projects/wk3/linkedlist.c
+++ practice/courses/coding for everyone - C and C++ specialization/projects/wk3/linkedlist.c
@@ -21,13 +21,36 @@ void print_list (node_t *head)
     printf ("\n");
 }
 
+void printlist (node_t *head)
+{
+    node_t *temporary = head;
+    
+    while (temporary != NULL)
+    {
+        printf ("%d: ", temporary -> value);
+        temporary = temporary -> next;
+    }
+
+    printf ("\n");
+}
+
 int main ()
 {
     node_t n1, n2, n3;
     node_t k1, k2, k3;
     node_t *head;
+    node_t *headd;
 
     n1.value = 45;
+    n2.value = 34;
+    n3.value = 676;
+
+    headd = &n3;
+    n3.value = &n2;
+    n2.value = &n1;
+    n1.value = NULL;
+
+    print_list (headd);
 
     k1.word = "H";
     k2.word = "W";
@@ -39,7 +62,8 @@ int main ()
     k2.word = &k1;
     k1.word = NULL; // this is where to stop
 
-    print_list (head);
+    //print_list (head);
+    printlist (headd);
     
     return 0;
 }
