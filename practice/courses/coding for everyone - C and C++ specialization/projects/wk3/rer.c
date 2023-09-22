fd3432e5781dd64307993e5d7debe3a4acea5b64
Chosen808 <learnkingdom@gmail.com>
2023-09-22 15:31:46 -0400

    Commit - 22923 - 1531 - 1

--- practice/courses/coding for everyone - C and C++ specialization/projects/wk3/rer.c
+++ practice/courses/coding for everyone - C and C++ specialization/projects/wk3/rer.c
@@ -7,11 +7,30 @@ main ()
     int target = 9;
     int answer = 0;
 
-    for (int i = 0, k = 1; i < 6, k < 7; i++, k++)
+    for (int i = 0, k = 0; i < 16, k < 0; i++)
     {
-        answer = nums[i] + nums[k];
+        /*
+        printf ("I = %d k = %d \n", i, k);
+        answer = nums[0] + nums[i];
+        printf ("Answer = %d \n", answer);
+        answer = nums[1] + nums[i];
+        printf ("Answer = %d \n", answer);
+        answer = nums[2] + nums[i];
+        printf ("Answer = %d \n", answer);
+        answer = nums[3] + nums[i];*/
 
-        if (answer == target)
+        answer = nums[k] + nums[i];
+        printf ("I = %d K = %d \n", i, k);
+
+        printf ("Answer = %d \n", answer);
+
+        if (answer != target)
+        {
+            k++;
+            printf ("K = %d -- \n", k);
+        }
+
+        else if (answer == target)
         {
             printf ("The solution has been found \n");
         }
