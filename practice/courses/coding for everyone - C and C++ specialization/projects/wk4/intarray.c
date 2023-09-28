0965c866378002a09ba537f4555149818155c61e
Chosen808 <learnkingdom@gmail.com>
2023-09-28 17:02:05 -0400

    Commit - 28923 - 1701

--- practice/courses/coding for everyone - C and C++ specialization/projects/wk4/intarray.c
+++ practice/courses/coding for everyone - C and C++ specialization/projects/wk4/intarray.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #define MAX_HW 20
 
-void print_file (FILE *in, int data[], int d_2[], int *size, int s)
+void print_file (FILE *in, int data[], int d_2[], int *size, int *s)
 {
     *size = 0;
+    *s = 0;
 
     while (fscanf (in, "%d", &data[*size]) == 1)
     {
         (*size)++;
 
-        s = data[0];
+        *s = data[0];
     }
 
-    for (int i = 0, k = 1; i < s, k < s + 1; i++, k++)
+    for (int i = 0, k = 1; i < *s, k < *s + 1; i++, k++)
     {
         d_2[i] = data[k];
     }
@@ -35,24 +35,38 @@ void print_data (int data[], int size)
     }
 }
 
+double average (int data_2[], int s)
+{
+    int max = 0;
+    double avg = 0;
+
+    for (int i = 0; i < s; i++)
+    {
+        max += data_2[i];
+
+        printf ("%d \n", max );
+
+        printf ("MAX = %d \n", data_2[i]);
+    }
+
+    return (avg = max / s);
+}
+
 main ()
 {
     FILE *in;
-    int data[20];
-    int data_2[0];
-    int size = 20;
-
-    int s = 0;
-
-    printf ("Y \n");
+    int data[MAX_HW];
+    int data_2[MAX_HW];
+    int size = MAX_HW;
+    int s = MAX_HW;
 
     in = fopen ("text", "r");
 
-    printf ("T \n");
-
-    print_file (in, data, data_2, &size, s);
+    print_file (in, data, data_2, &size, &s);
 
     print_data (data_2, s);
+
+    printf ("AVERAGE =  %f \n", average (data_2, s));
 
     return 0;
 }
