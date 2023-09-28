f4abffadd1d36cd7ffadfdc31372c40c71570b0a
Chosen808 <learnkingdom@gmail.com>
2023-09-28 17:34:34 -0400

    Commit - 28923 - 1734

--- practice/courses/coding for everyone - C and C++ specialization/projects/wk4/intarray.c
+++ practice/courses/coding for everyone - C and C++ specialization/projects/wk4/intarray.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MAX_HW 20
 
-void print_file (FILE *in, int data[], int d_2[], int *size, int *s)
+void print_file (FILE *in, int data[], int d_2[], int *size, double *s)
 {
     *size = 0;
     *s = 0;
@@ -22,7 +24,7 @@ void print_file (FILE *in, int data[], int d_2[], int *size, int *s)
     fclose (in);
 }
 
-void print_data (int data[], int size)
+void print_data (int data[], double size)
 {
     for (int i = 0; i < size; i++)
     {
@@ -35,38 +37,63 @@ void print_data (int data[], int size)
     }
 }
 
-double average (int data_2[], int s)
+double average (int data_2[], double s)
 {
     int max = 0;
-    double avg = 0;
+    double avg = 0.0;
 
     for (int i = 0; i < s; i++)
     {
         max += data_2[i];
-
-        printf ("%d \n", max );
-
-        printf ("MAX = %d \n", data_2[i]);
     }
 
     return (avg = max / s);
 }
 
-main ()
+void file_out (int data_2[], int s, FILE *out)
 {
-    FILE *in;
+    char c[50];
+
+    for (int i = 0; i < s; i++)
+    {
+        //c = getc (data_2[i]);
+
+        //strcpy (c, data_2[i]);
+
+        fwrite (data_2[i], s, 1, out);
+
+        //putc (c, out);
+    }
+}
+
+main (int argc, char *argv[])
+{
+    FILE *in, *out;
+
     int data[MAX_HW];
     int data_2[MAX_HW];
     int size = MAX_HW;
-    int s = MAX_HW;
+    double s = MAX_HW;
 
-    in = fopen ("text", "r");
+    if (argc != 3)
+    {
+        printf ("Need executable inputfile outputfile \n");
+
+        exit (1);
+    }
+
+    in = fopen (argv[1], "r+");
+    out = fopen (argv[2], "w+");
 
     print_file (in, data, data_2, &size, &s);
 
     print_data (data_2, s);
 
-    printf ("AVERAGE =  %f \n", average (data_2, s));
+    printf ("\n");
+
+    printf ("AVERAGE =  %.4f \n", average (data_2, s));
+
+    file_out (data_2, s, out);
 
     return 0;
 }
