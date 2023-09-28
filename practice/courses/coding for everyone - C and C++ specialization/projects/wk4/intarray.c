f59f44470c42cc6e42d122b9a81902a4ec1911e8
Chosen808 <learnkingdom@gmail.com>
2023-09-28 16:16:09 -0400

    Commit - 28923 - 1616

--- practice/courses/coding for everyone - C and C++ specialization/projects/wk4/intarray.c
+++ practice/courses/coding for everyone - C and C++ specialization/projects/wk4/intarray.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_HW 20
+
 void print_file (FILE *in, int data[], int *size)
 {
     *size = 0;
-    
-    while (fscanf (in, "%d", &data[*size] == 1))
+
+    while (fscanf (in, "%d", &data[*size]) == 1)
     {
         (*size)++;
     }
@@ -13,19 +15,39 @@ void print_file (FILE *in, int data[], int *size)
     fclose (in);
 }
 
+void print_data (int data[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf ("%d \t", data[i]);
+
+        if (i == size -1)
+        {
+            printf ("\n");
+        }
+    }
+}
+
 main ()
 {
     FILE *in;
-    int data[5];
-    int size = 0;
+    int data[20];
+    int size = 20;
 
-    in = fopen ("myfile.txt", "r+");
+    printf ("Y \n");
+
+    in = fopen ("text", "r");
+
+    printf ("T \n");
 
     print_file (in, data, &size);
+
+    print_data (data, size);
 
     for (int i = 0; i < size; i++)
     {
         printf ("DATA = %d \n", data[i]);
     }
+
     return 0;
 }
