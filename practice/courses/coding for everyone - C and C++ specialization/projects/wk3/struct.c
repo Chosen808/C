22e450e66542275cc021a2dded34615a9ad83b3e
Chosen808 <learnkingdom@gmail.com>
2023-10-03 18:08:06 -0400

    Commit 31023 - 1807

--- practice/courses/coding for everyone - C and C++ specialization/projects/wk3/struct.c
+++ practice/courses/coding for everyone - C and C++ specialization/projects/wk3/struct.c
@@ -33,15 +33,21 @@ den *add_to_front (char arr, den *h)
 
 den *array_to_list (char array[], char arr[], int size, den *h, char *title)
 {
-    den *head;
+    den *head = create_name (arr[0]);
 
-    for (int i = 1; i < size; i++)
+    for (int i = 0; i < size; i++)
     {
-        //strcpy (arr, array[i]);
+        printf ("Array = %s \n", array[i]);
 
-        head = create_name (arr[0]);
+        printf ("Arr = %s \n", arr);
+        strcpy (arr, array[i]);
 
-        head = add_to_front (arr[i], head);
+        for (int i = 1; i < size; i++)
+        {
+            head = add_to_front (arr[i], head);
+        }
+
+        //head = add_to_front (arr[i], head);
     }
 
     printf ("%s \n", title);
@@ -60,7 +66,7 @@ main ()
 
     den *head = NULL;
 
-    char arr[10];
+    char arr[10] = {" "};
     char array[10][10];
 
     for (int i = 0; i < 10; i++)
@@ -79,17 +85,18 @@ main ()
         }
     }
 
+/*
     for (int i = 0, k = 1; i < 10, k < 11; i++, k++)
     {
-        printf ("I = %d \n", i);
+        //printf ("I = %d \n", i);
         strcpy (arr, array[i]);
 
         printf ("Arr = %s %d\n", arr, k);
 
 
-    }
+    }*/
 
-    //head = array_to_list (array, arr, 10, head, "Test");
+    head = array_to_list (array, arr, 10, head, "Test");
 
     return 0;
 }
