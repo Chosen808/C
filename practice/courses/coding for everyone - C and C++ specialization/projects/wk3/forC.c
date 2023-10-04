5c81b81c838a730da9475d6e1fcf66e3ab30042e
Chosen808 <learnkingdom@gmail.com>
2023-10-04 18:38:06 -0400

    Commit - 41023 - 1838

--- practice/courses/coding for everyone - C and C++ specialization/projects/wk3/forC.c
+++ practice/courses/coding for everyone - C and C++ specialization/projects/wk3/forC.c
@@ -2,23 +2,57 @@
 
 main ()
 {
+    /*
     for (int i = 0; i < 20 || i == 20; i++)
     {
-        printf ("I = %d \n", i);
+        printf ("*I = %d \n", i);
 
         if (i == 5)
         {
-            printf ("i = %d \n", i);
+            printf ("*i = %d \n", i);
         }
 
         else if (i == 9)
         {
-            printf ("i = %d \n", i);
+            printf ("*i = %d \n", i);
         }
 
         else if (i == 20)
         {
-            printf ("i = %d \n", i);
+            printf ("*i = %d \n", i);
+        }
+    }
+
+    for (int i = 0; i < 20 || i == 20; i++)
+    {
+        printf ("I = %d \n", i);
+
+        if (i == 20)
+        {
+            for (; i > 0 || i == 0; i--)
+            {
+                printf ("i = %d \n", i);
+            }
+
+            return 0;
+        }
+    }*/
+
+    int array[5][5];
+
+    printf ("Please enter: \n");
+
+    for (int i = 0, k = 0; i < 5, k < 5; i++, k++)
+    {
+        printf ("Enter number %d \n", i);
+        scanf ("%d", &array[i][i]);
+
+        if (i == 4)
+        {
+            for (int i = 0, k = 0; i < 5, k < 5; i++, k++)
+            {
+                printf ("array = %d \n", array[i][i]);
+            }
         }
     }
 
