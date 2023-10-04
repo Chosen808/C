38467c3e5daea68b821ef95ef9b04eebad7af6b5
Chosen808 <learnkingdom@gmail.com>
2023-10-04 17:38:07 -0400

    Commit - 41023 - 1738

--- practice/courses/coding for everyone - C and C++ specialization/projects/wk3/forC.c
+++ practice/courses/coding for everyone - C and C++ specialization/projects/wk3/forC.c
@@ -5,6 +5,21 @@ main ()
     for (int i = 0; i < 20 || i == 20; i++)
     {
         printf ("I = %d \n", i);
+
+        if (i == 5)
+        {
+            printf ("i = %d \n", i);
+        }
+
+        else if (i == 9)
+        {
+            printf ("i = %d \n", i);
+        }
+
+        else if (i == 20)
+        {
+            printf ("i = %d \n", i);
+        }
     }
 
     return 0;
