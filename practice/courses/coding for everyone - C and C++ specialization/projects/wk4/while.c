2d8e3b398771f2210410dc2ae27634181673f362
Chosen808 <learnkingdom@gmail.com>
2023-10-08 00:23:02 -0400

    Commit - 81023 - 1222

---
+++ practice/courses/coding for everyone - C and C++ specialization/projects/wk4/while.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+main ()
+{
+    int i = 0, k = 0;
+
+    while (i <= 20)
+    {
+        i++;
+        //k++;
+
+        printf ("%d * 5 = %d \n", i, i * 5);
+    }
+
+    return 0;
+}
