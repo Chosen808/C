f6a7b067dae2cf00dfbf3e78713960b1a9c0d64c
Chosen808 <learnkingdom@gmail.com>
2023-10-08 11:51:33 -0400

    Commit - 81023 - 1151

--- practice/courses/coding for everyone - C and C++ specialization/projects/wk4/stars.c
+++ practice/courses/coding for everyone - C and C++ specialization/projects/wk4/stars.c
@@ -10,7 +10,8 @@ main ()
 
     for (int i = 0; i <= rows; i++)
     {
-        for (int j= 0; j <= i; j++)
+
+        for (int j= 0; j <= i; j++) // j will print each time to match the increment of i
         {
             printf ("* ");
         }
