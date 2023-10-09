80d792e6fbf4b7c7a733a799d7133d3c3b766789
Chosen808 <learnkingdom@gmail.com>
2023-10-09 19:06:31 -0400

    Commit - 91023 - 1906

--- practice/courses/coding for everyone - C and C++ specialization/projects/howto/pointers/pointerHan.c
+++ practice/courses/coding for everyone - C and C++ specialization/projects/howto/pointers/pointerHan.c
@@ -1,9 +1,28 @@
+/*
+2. Write a program in C to demonstrate how to handle pointers in a program.
+Expected Output :
+
+Address of m : 0x7ffcc3ad291c
+ Value of m : 29                                                                                              
+                                                                                                              
+ Now ab is assigned with the address of m.                                                                    
+ Address of pointer ab : 0x7ffcc3ad291c                                                                       
+ Content of pointer ab : 29                                                                                   
+                                                                                                              
+ The value of m assigned to 34 now.                                                                           
+ Address of pointer ab : 0x7ffcc3ad291c                                                                       
+ Content of pointer ab : 34                                                                                   
+                                                                                                              
+ The pointer variable ab is assigned with the value 7 now.                                                    
+ Address of m : 0x7ffcc3ad291c                                                                                
+ Value of m : 7 
+ */
+
 #include <stdio.h>
 
 
 main ()
 {
-    //int i = 29;
     int *m = 29;
     int *ab = &m;
 
@@ -22,5 +41,27 @@ main ()
 
     printf ("&m %p \n", &m);  // address of m
     printf ("m = %d \n", m);  // value of m
+
+
+    int n = 29;
+    int *cd = &n;
+
+    printf ("&n = %p \n", &n);  // address of n
+    printf ("n = %d \n", n);    // value of n
+
+    printf ("cd->n = %p \n", cd);   // address of cd
+    printf ("*cd = %d \n", *cd);    // content of cd
+
+    n = 34;
+
+    printf ("cd->n = %p \n", cd);   // address of cd
+    printf ("*cd = %d \n", *cd);    // content of cd
+
+    *cd = 7;
+
+    printf ("&n = %p \n", &n);  // address of n
+    printf ("n = %d \n", n);    // value of n
+
+    return 0;
 
 }
