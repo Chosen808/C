20e143cf24413df2140a30c46809a83ba2d3d992
Chosen808 <learnkingdom@gmail.com>
2023-10-09 17:45:20 -0400

    Commit - 91023 - 1745

---
+++ practice/courses/coding for everyone - C and C++ specialization/projects/howto/pointers/pointerHan.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+main ()
+{
+    //int i = 29;
+    int *m = 29;
+    int *ab = &m;
+
+    printf ("&m = %p \n", &m);  // address of m
+    printf ("*m = %d \n", m);   // value of m
+
+    printf ("ab->m = %p \n", ab);   // address of pointer ab
+    printf ("*ab = %d \n", *ab);    // content of pointer ab
+
+    m = 34;
+
+    printf ("ab->m %p \n", ab); // address of pointer ab
+    printf ("*ab = %d \n", *ab);    // content of pointer ab
+
+    m = 7;
+
+    printf ("&m %p \n", &m);  // address of m
+    printf ("*m = %d \n", m);  // value of m
+
+}
