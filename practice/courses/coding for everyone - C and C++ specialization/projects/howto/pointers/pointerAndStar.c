3ffce530e765926aa775106101d9d423351f5331
Chosen808 <learnkingdom@gmail.com>
2023-10-10 09:38:51 -0400

    Commit - 101023 - 938

--- practice/courses/coding for everyone - C and C++ specialization/projects/howto/pointers/pointerAndStar.c
+++ practice/courses/coding for everyone - C and C++ specialization/projects/howto/pointers/pointerAndStar.c
@@ -1,3 +1,38 @@
+/*
+3. Write a program in C to demonstrate the use of the &(address of) and *(value at address) operators.
+Expected Output :
+
+Pointer : Demonstrate the use of & and * operator :                                                          
+--------------------------------------------------------                                                      
+ m = 300                                                                                                      
+ fx = 300.600006                                                                                              
+ cht = z                                                                                                      
+                                                                                                              
+ Using & operator :                                                                                           
+-----------------------                                                                                       
+ address of m = 0x7ffda2eeeec8                                                                                
+ address of fx = 0x7ffda2eeeecc                                                                               
+ address of cht = 0x7ffda2eeeec7                                                                              
+                                                                                                              
+ Using & and * operator :                                                                                     
+-----------------------------                                                                                 
+ value at address of m = 300                                                                                  
+ value at address of fx = 300.600006                                                                          
+ value at address of cht = z                      
+
+Using only pointer variable :                                                                                
+----------------------------------                                                                            
+ address of m = 0x7ffda2eeeec8                                                                                
+ address of fx = 0x7ffda2eeeecc                                                                               
+ address of cht = 0x7ffda2eeeec7                                                                              
+                                                                                                              
+ Using only pointer operator :                                                                                
+----------------------------------                                                                            
+ value at address of m = 300                                                                                  
+ value at address of fx= 300.600006                                                                           
+ value at address of cht= z 
+ */
+
 #include <stdio.h>
 
 
@@ -15,9 +50,17 @@ main ()
     printf ("&fx = %p \n", &fx);    // address of fx
     printf ("&cht = %p \n", &cht);  // address of cht
 
+    printf ("*(&m) = %d \n", *(&m));  // value at m
+    printf ("*(fx) = %f \n", *(&fx));    // value at fx
+    printf ("*(&cht) = %c \n", *(&cht));    // value at cht
+
+    printf ("x->m = %p \n", x); // address of m
+    printf ("y->fx = %p \n", y);    // address of fx
+    printf ("z-> cht = %p \n", z);  // address of cht
+
     printf ("*x = %d \n", *x);  // value at m
-    printf ("*y = %f \n", *y);    // value at fx
-    printf ("*cht = %c \n", *z);    // value at cht
+    printf ("*fx = %f \n", y);  // value at fx
+    printf ("*z = %c \n", *z);  // value at cht
 
 
 
