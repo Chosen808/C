172c3c0cb7d878f2d743363e783d0071909580e0
Chosen808 <learnkingdom@gmail.com>
2023-10-11 09:10:53 -0400

    Commit 111023 - 910

--- practice/courses/coding for everyone - C and C++ specialization/projects/howto/pointers/pointerGrLs.c
+++ practice/courses/coding for everyone - C and C++ specialization/projects/howto/pointers/pointerGrLs.c
@@ -1,3 +1,13 @@
+/*
+6. Write a program in C to find the maximum number between two numbers using a pointer.
+Test Data :
+Input the first number : 5
+Input the second number : 6
+Expected Output :
+
+6 is the maximum number. 
+*/
+
 #include <stdio.h>
 
 int maxim (int *c, int *d);
