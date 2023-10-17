e46ae860a584e3c850cb95a2be05132d7ca6706f
Chosen808 <learnkingdom@gmail.com>
2023-10-17 02:12:57 -0400

    Commit 171023 212

---
+++ practice/courses/coding for everyone - C and C++ specialization/projects/wk4/intergerRead.c
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_NUM 100
+
+typedef int DATA_t;
+
+/*
+This struct will hold the variables that will process the data for the binary tree
+*/
+typedef struct node
+{
+    DATA_t d;
+
+    struct node *left;
+    struct node *right;
+};
+
+typedef struct node NODE_t;
+
+typedef NODE_t *BTREE;
+
+/*
+This function will walk the tree "inorder" and print these values to the screen
+*/
+void inorder (BTREE root)
+{
+    if (root != NULL)
+    {
+        inorder (root->left);
+
+        printf ("%d ", root->d);
+
+        inorder (root->right);
+    }
+}
+
+BTREE newNode (void)
+{
+    return (malloc (sizeof (NODE_t)));
+}
+
+BTREE initNode (DATA_t d1, BTREE p1, BTREE p2)
+{
+    BTREE t;
+
+    t = newNode ();
+    t->d = d1;
+    t->left = p1;
+    t->right = p2;
+
+    return t;
+}
+
+BTREE createTree (DATA_t a[], int i, int size)
+{
+    if (i >= size)
+    {
+        return NULL;
+    }
+
+    else 
+    {
+        return (initNode (a[i], createTree (a, 2 * i + 1, size), createTree (a, 2 * i + 2, size)));
+    }
+}
+
+/*
+This function will read the input file which is put in at the terminal
+and pass the data to int data[]. int data[] will then pass the selected
+numbers to int d2[]. The first number in the file will be passed to
+double *s which the number of number to be processed.
+*/
+void arrayRead (FILE *in, int data[], int d2[], int *size, int *s)
+{
+    *size = 0;  // this will store the number of digits in the file
+    *s = 0; // this will store the number of digits to be processed
+
+    /*
+    This function passes the data which comes from the file which is
+    passed to the pointer FILE *in and stores it in an array int data[].
+    It also counts the amount of numbers and stores in int *size
+    */
+    while (fscanf (in, "%d", &data[*size]) == 1)
+    {
+        (*size)++;  // this stores how many numbers are in the file and passes it to int *size
+
+        *s = data[0];   // this will pass the first number in array to tell how many to read
+    }
+
+    /*
+    This statement will pass the number to be read from int data[] to int data2
+    */
+    for (int i = 0, k = 1; i < *s, k < *s + 1; i++, k++)
+    {
+        d2[i] = data[k];
+    }
+
+    fclose (in);    // this will close the file
+}
+
+
+/*
+This function will print the data that is stored in the array int data[]
+*/
+void printData (int data2[], int size)
+{
+    for (int i = 0; i < size; i++)  // this for loop print the data stored in int data[]
+    {
+        printf ("%d \t", data2[i]); // this prints the numbers stored in int data[]
+
+        if (i == size - 1)  // this statement will print a new line when i is finished counting
+        {
+            puts ("\n");
+        }
+    }
+}
+
+
+main (int argc, char *argv[])
+{
+    FILE *in;
+
+    int data[MAX_NUM];
+    int data2[MAX_NUM];
+    int size = MAX_NUM;
+    int s = MAX_NUM;
+
+    BTREE b;
+
+
+    if (argc != 2)
+    {
+        printf ("Need executable input/output file \n");
+
+        exit (1);
+    }
+
+    in = fopen (argv[1], "r+");
+
+    arrayRead (in, data, data2, &size, &s);
+
+    printData (data2, s);
+
+    b = createTree (data2, 0, s);
+
+    printf ("INORDER \n");
+
+    inorder (b);
+
+    puts ("\n\n");
+
+    return 0;
+}
