6ad998d86084a22e065e25032e7b3fb4a9381f0b
Chosen808 <learnkingdom@gmail.com>
2023-10-17 12:28:39 -0400

    Commit 171023 1228

--- practice/courses/coding for everyone - C and C++ specialization/projects/wk4/intergerRead.c
+++ practice/courses/coding for everyone - C and C++ specialization/projects/wk4/intergerRead.c
@@ -1,3 +1,17 @@
+/*
+Thank you Lord Jesus for building this program
+
+Open and read a file of integer pairs into an array that is created with the first integer 
+telling you how many to read.  
+
+So  4 9  11  4  5    would mean create an integer array size 4 and read into data[].  
+
+Write a routine that places these values into a binary tree structure. Then walk the tree 
+“inorder” and print these values to the screen.
+
+Submit your work in a text file. 
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
