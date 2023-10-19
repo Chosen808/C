5f6663868f6549b075d099eb147151408892cec9
Chosen808 <learnkingdom@gmail.com>
2023-10-18 21:32:00 -0400

    Commit 181023 2131

--- practice/udemy/cProgforBeg/arrays.c
+++ practice/udemy/cProgforBeg/arrays.c
@@ -9,5 +9,29 @@ main ()
 
     printf ("Value of Array Index 4 = %d \n", arrayAccess);
 
+    for (int i = 0; i < 6; i++)
+    {
+        printf ("ArrayID[%d] = %d \n", i, array[i]);
+    }
+
+    int mark[10];
+
+    int i, n, sum = 0;
+
+    printf ("How many subjects: ");
+    scanf ("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        printf ("Total marks of subject %d ", i + 1);
+        scanf ("%d", &mark[i]);
+
+        sum += mark[i];
+    }
+
+    printf ("Sum = %d \n", sum);
+
+
+
     return 0;
 }
