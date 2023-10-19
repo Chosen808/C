528f9647cfbb25a8fbb8b4c5627ec65724ce4c71
Chosen808 <learnkingdom@gmail.com>
2023-10-18 21:16:16 -0400

    Commit 181023 2116

---
+++ practice/udemy/cProgforBeg/arrays.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+main ()
+{
+    int array[6] = {34, 423, 12, 54, 5, 76};
+
+    int arrayAccess = array[4];
+
+    printf ("Value of Array Index 4 = %d \n", arrayAccess);
+
+    return 0;
+}
