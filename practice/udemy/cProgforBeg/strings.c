83187e710781976f00569a5d95dcc6c8750cecfc
Chosen808 <learnkingdom@gmail.com>
2023-10-22 16:02:20 -0400

    Commit 221023 1602

--- practice/udemy/cProgforBeg/strings.c
+++ practice/udemy/cProgforBeg/strings.c
@@ -1,34 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 main ()
 {
     char a[] = "The";
-    char b[5] = "Then";
-    char f[7] = "Ylas";
-    char c[6] = {'a', 'e', 'i', 'o', 'u'};
+    char b[4] = "Then";
+    //char f[4] = "Ylas";
+    char c[5] = {'a', 'e', 'i', 'o', 'u'};
+    char n[] = "erter";
+    char f[4] = "Ylas";
 
     printf ("a = %s \n", a);
     printf ("b = %s \n", b);
     printf ("c = %s \n", c);
     printf ("f = %s \n", f);
+    printf ("n = %s \n", n);
 
     printf ("&a = %p \n", &a);
     printf ("&b = %p \n", &b);
     printf ("&c = %p \n", &c);
+    printf ("&f = %p \n", &f);
+    printf ("&n = %p \n", &n);
 
     char d[10];
 
     printf ("Please enter some letters: ");
-    scanf ("%10s", &d);
+    fgets (d, 10, stdin);
 
     printf ("d = %s \n", d);
 
-    char h[] = " ";
+    printf ("\n");
+
+    char h[200];
 
     printf ("Enter more characters: ");
-    scanf ("%s", &h);
+    fgets (h, 200, stdin);
 
     printf ("h = %s \n", h);
 
