ee071e5da9ae75e04164e82bc6e268ac9b3531bb
Chosen808 <learnkingdom@gmail.com>
2023-10-29 18:20:50 -0400

    Commit 291023 1820

--- practice/udemy/cProgforBeg/pointers.c
+++ practice/udemy/cProgforBeg/pointers.c
@@ -10,5 +10,14 @@ main ()
     printf ("a = %d \n", a);
     printf ("b->a = %p \n", b);
 
+    int array[5] = {4, 5, 7, 9, 12};
+
+    int *p = &array;
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf ("p->array[%d] = %p \n", i, p[i]);
+    }
+
     return 0;
 }
