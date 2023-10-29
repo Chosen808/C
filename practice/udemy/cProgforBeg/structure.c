ffb3a77704339a5e1236a505855def04c1f4ba3e
Chosen808 <learnkingdom@gmail.com>
2023-10-29 19:08:22 -0400

    Commit 291023 1908

---
+++ practice/udemy/cProgforBeg/structure.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct person 
+{
+    int age;
+    char name[5][6];
+    float salary;
+
+    struct id_t *id;
+} id_t;
+
+
+main ()
+{
+    id_t *info;
+
+    printf ("TEST \n");
+
+    info->age = 20;
+
+    printf ("TEST2 \n");
+
+    strcpy (info->name[0], "Thomas");
+
+    printf ("TEST3 \n");
+
+    info->salary = 20000.50;
+
+    printf ("TEST4 \n");
+
+    printf ("The age is %d \n", info->age);
+    printf ("The name is %s \n", info->name[0]);
+    printf ("The salary is $%f \n", info->salary);
+
+    return 0;
+}
