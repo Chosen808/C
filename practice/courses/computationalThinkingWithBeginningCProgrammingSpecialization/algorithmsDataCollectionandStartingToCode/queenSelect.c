1aebaa1bb4caefe35c32380acd5961dbc7025567
Chosen808 <learnkingdom@gmail.com>
2023-11-07 21:22:21 -0400

    Commit 71123 2122

---
+++ practice/courses/computationalThinkingWithBeginningCProgrammingSpecialization/algorithmsDataCollectionandStartingToCode/queenSelect.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define CARDSDRAWS 10000
+
+int randNum ()
+{
+    time_t t;
+    srand ((unsigned) time(&t));
+
+    pull:
+    int draw = rand() % 60;
+
+    if (draw >= 52)
+    {
+        goto pull;
+    }
+
+    else 
+    {
+        return draw;
+    }
+}
+
+
+main ()
+{
+    char cards[52][52] = {"Ace Spade", "Spade Two", "Spade Three", "Spade Four", "Spade Five", "Spade Six", "Spade Seven", "Spade Eight", "Spade Nine", "Spade Ten", "Spade Jack", "Spade Queen", "Spade King",
+    "Ace Heart", "Heart Two", "Heart Three", "Heart Four", "Heart Five", "Heart Six", "Heart Seven", "Heart Eight", "Heart Nine", "Heart Ten", "Heart Jack", "Heart Queen", "Heart King",
+    "Ace Diamond", "Diamond Two", "Diamond Three", "Diamond Four", "Diamond Five", "Diamind Six", "Diamond Seven", "Diamond Eight", "Diamond Nine", "Diamond Ten", "Diamond Jack", "Diamond Queen", "Diamond King",
+    "Ace Clubs", "Clubs Two", "Clubs Three", "Clubs Four", "Clubs Five", "Clubs Six", "Clubs Seven", "Clubs Eight", "Clubs Nine", "Clubs Ten", "Clubs Jack", "Club Queen", "Clubs King"};
+
+    int card;
+    int temp;
+    int cycles[CARDSDRAWS];
+
+    draw:
+    card = randNum ();
+
+    if (card != 11)
+    {
+        printf ("Your drew %s \n", cards[card]);
+        printf ("Please draw again, press enter: ");
+
+        getchar ();
+
+        goto draw;
+    }
+
+    else if (card == 11)
+    {
+        return 0;
+    }
+
+    return 0;
+}
