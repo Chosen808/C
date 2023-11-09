a88b25776bf643f7d24ec9c38952176567ba6855
Chosen808 <learnkingdom@gmail.com>
2023-11-09 14:02:19 -0400

    Commit 91123 1402

--- practice/courses/computationalThinkingWithBeginningCProgrammingSpecialization/algorithmsDataCollectionandStartingToCode/queenSelect.c
+++ practice/courses/computationalThinkingWithBeginningCProgrammingSpecialization/algorithmsDataCollectionandStartingToCode/queenSelect.c
@@ -4,6 +4,22 @@
 
 #define CARDSDRAWS 1000000
 
+typedef struct cardHand
+{
+    int card;
+    int temp;
+    unsigned long long cycles[CARDSDRAWS];
+    int cardCount;
+    int index;
+} hand_t;
+
+char cards[52][52] = 
+{"Ace Spade", "Spade Two", "Spade Three", "Spade Four", "Spade Five", "Spade Six", "Spade Seven", "Spade Eight", "Spade Nine", "Spade Ten", "Spade Jack", "Spade Queen", "Spade King",
+    "Ace Heart", "Heart Two", "Heart Three", "Heart Four", "Heart Five", "Heart Six", "Heart Seven", "Heart Eight", "Heart Nine", "Heart Ten", "Heart Jack", "Heart Queen", "Heart King",
+    "Ace Diamond", "Diamond Two", "Diamond Three", "Diamond Four", "Diamond Five", "Diamind Six", "Diamond Seven", "Diamond Eight", "Diamond Nine", "Diamond Ten", "Diamond Jack", "Diamond Queen", "Diamond King",
+    "Ace Clubs", "Clubs Two", "Clubs Three", "Clubs Four", "Clubs Five", "Clubs Six", "Clubs Seven", "Clubs Eight", "Clubs Nine", "Clubs Ten", "Clubs Jack", "Club Queen", "Clubs King"
+    };
+
 int randNum ()
 {
     time_t t;
@@ -26,148 +42,62 @@ int randNum ()
 
 main ()
 {
-    char cards[52][52] = {"Ace Spade", "Spade Two", "Spade Three", "Spade Four", "Spade Five", "Spade Six", "Spade Seven", "Spade Eight", "Spade Nine", "Spade Ten", "Spade Jack", "Spade Queen", "Spade King",
-    "Ace Heart", "Heart Two", "Heart Three", "Heart Four", "Heart Five", "Heart Six", "Heart Seven", "Heart Eight", "Heart Nine", "Heart Ten", "Heart Jack", "Heart Queen", "Heart King",
-    "Ace Diamond", "Diamond Two", "Diamond Three", "Diamond Four", "Diamond Five", "Diamind Six", "Diamond Seven", "Diamond Eight", "Diamond Nine", "Diamond Ten", "Diamond Jack", "Diamond Queen", "Diamond King",
-    "Ace Clubs", "Clubs Two", "Clubs Three", "Clubs Four", "Clubs Five", "Clubs Six", "Clubs Seven", "Clubs Eight", "Clubs Nine", "Clubs Ten", "Clubs Jack", "Club Queen", "Clubs King"};
+    int j = 0;
 
-    int card;
-    int temp = 2;
-    long cycles[CARDSDRAWS];
-    int cardCount;
-    int index = 0;
+    hand_t suite;
+    suite.temp = 2;
 
     draw:
-    card = randNum ();
+    suite.card = randNum ();
 
-    if (card != 11 && card != 24 && card != 37 && card != 50)
+    if (suite.card != 11 && suite.card != 24 && suite.card != 37 && suite.card != 50)
     {
 
-        for (int i = 1; i < temp; i++)
-        {
-            //++i;
-
-            //printf ("I = %d \n", i);
-
-            //temp += i;
-
-            //cycles[index] = card;
-
-            //++i;
-
-            temp += i;
-            //index += i;
-
-            printf ("card = %d \n", card);
-
-            printf ("i = %d \n", i);
-
-            printf ("cycles[%d] = %ld \n", i, cycles[i]);
-
-            printf ("temp = %d \n", temp);
-
-            for (int j = 0; j < i; j++)
-            {
-                if (cycles[j] != card)
-                {
-                    cycles[index] = card;
-
-                    index += i;
-                    printf ("You drew a %s \n", cards[card]);
-
-                    printf ("Please draw again, press enter \n");
-
-                    getchar ();
-
-                    goto draw;
-                }
-
-                else if (cycles[j] == card)
-                {
-                    cycles[index] = card;
-
-                    index += i;
-
-                    printf ("TEST");
-
-                    goto draw;
-                }
-            }
-        }
-    }  
-
-    else 
-    {
-        printf ("You drew a %s \n", cards[card]);
-
-        return 0;
-    }  
-
-/*
-    if (card != 11 && card != 24 && card != 37 && card != 50 )
-    {
-        for (int i = 0; i < temp;)
+        for (int i = 0; i < suite.temp;)
         {
             ++i;
 
-            printf ("i = %d \n", i);
+            suite.temp += i;
 
-            printf ("cycles[%d] = %ld \n", i, cycles[i]);
-
-            temp += i;
-
-            cycles[index] = card;
-
-            index += i;
-
-            printf ("card = %d \n", card);
-
-            printf ("temp = %d \n", temp);
-
-            for (int j = 0; j < temp; j++)
+            while (suite.cycles[j] != suite.card && j < suite.temp)
             {
-                printf ("j 1 = %d \n", j);
+                j++;
+            }
 
-                cycles[j];
+            if (suite.cycles[j] != suite.card)
+            {
+                suite.cycles[suite.index] = suite.card;
 
-                if (card == cycles[j])
-                {
-                    printf ("j 2 = %d \n", j);
-                    printf ("2cycles[%d] = %ld \n", j, cycles[j]);
-                    //cycles[j] = card;
+                suite.index += i;
 
-                    goto draw;
+                printf("You drew a %s \n", cards[suite.card]);
 
-                    //printf ("You drew a %s \n", cards[card]);
-                    //printf ("Please draw again, press enter \n");
+                printf ("Please draw again, press enter \n");
 
-                    //getchar ();
+                j = 0;
 
-                    //goto draw;
-                }
+                getchar ();
 
-                else
-                {
-                    //cycles[j] = card;
-                    //printf ("PLLL \n");
-                    printf ("You drew a %s \n", cards[card]);
-                    printf ("Please draw again, press enter \n");
+                goto draw;
 
-                    getchar ();
 
-                    goto draw;
-                }
+            }
+
+            else if (suite.cycles[j] == suite.card)
+            {
+                j = 0;
+    
+                goto draw;
             }
         }
-        
     }
 
-    else
+    else 
     {
-        printf ("You drew a %s \n", cards[card]);
+        printf ("You drew a %s \n", cards[suite.card]);
 
         return 0;
-    }*/
+    }  
 
     return 0;
 }
