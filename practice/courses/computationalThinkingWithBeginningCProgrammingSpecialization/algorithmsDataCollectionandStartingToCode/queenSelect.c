0a15b20c2177951731b70c5218e6eeab9812628f
Chosen808 <learnkingdom@gmail.com>
2023-11-09 14:58:09 -0400

    Commit 91123 1458

--- practice/courses/computationalThinkingWithBeginningCProgrammingSpecialization/algorithmsDataCollectionandStartingToCode/queenSelect.c
+++ practice/courses/computationalThinkingWithBeginningCProgrammingSpecialization/algorithmsDataCollectionandStartingToCode/queenSelect.c
@@ -34,15 +34,16 @@ v1.0
 
 #define CARDSDRAWS 1000000
 
+// Data is held here for the cards
 typedef struct cardHand
 {
-    int card;
-    int temp;
-    unsigned long long cycles[CARDSDRAWS];
-    int cardCount;
-    int index;
+    int cardNumber; // the card number that is generated from randNum () will store here
+    int temp;   // this will be used to create a counter for i and j based of the number of times cycles is initialized
+    unsigned long long cycles[CARDSDRAWS]; // used to store the numbers that a generated and stored in cardNumber
+    int index;  // this is used to create the counter for cycles to be initalized by cardNumber
 } hand_t;
 
+// Cards are stored here
 char cards[52][52] = 
 {"Ace Spade", "Spade Two", "Spade Three", "Spade Four", "Spade Five", "Spade Six", "Spade Seven", "Spade Eight", "Spade Nine", "Spade Ten", "Spade Jack", "Spade Queen", "Spade King",
     "Ace Heart", "Heart Two", "Heart Three", "Heart Four", "Heart Five", "Heart Six", "Heart Seven", "Heart Eight", "Heart Nine", "Heart Ten", "Heart Jack", "Heart Queen", "Heart King",
@@ -50,25 +51,29 @@ char cards[52][52] =
     "Ace Clubs", "Clubs Two", "Clubs Three", "Clubs Four", "Clubs Five", "Clubs Six", "Clubs Seven", "Clubs Eight", "Clubs Nine", "Clubs Ten", "Clubs Jack", "Club Queen", "Clubs King"
     };
 
+// Randon numbers are generated here between 0 nd 52
 int randNum ()
 {
     time_t t;
-    srand ((unsigned) time(&t));
+    srand ((unsigned) time(&t));    // initializes the rand () function
 
     pull:
-    int draw = rand() % 60;
+    int draw = rand() % 60; // random number are generated here and stored in draw
 
+    // If a number is greater than or equal to 52 another random number will be generated
     if (draw >= 52)
     {
         goto pull;
     }
 
+    // If the number fall between 0 and 52 it will be returned
     else 
     {
         return draw;
     }
 }
 
+// This is where the card will be drawn
 int cardDraw ()
 {
     hand_t suite;
@@ -77,28 +82,31 @@ int cardDraw ()
     suite.temp = 2;
 
     draw:
-    suite.card = randNum ();
+    suite.cardNumber = randNum ();  // random number are generated and stored in cardNumber
 
-    if (suite.card != 11 && suite.card != 24 && suite.card != 37 && suite.card != 50)
+    // If the number generated is not equal to any of these, which are queens this statement will execute
+    if (suite.cardNumber != 11 && suite.cardNumber != 24 && suite.cardNumber != 37 && suite.cardNumber != 50)
     {
         for (int i = 0; i < suite.temp;)
         {
             ++i;
 
-            suite.temp += i;
+            suite.temp += i;    // i will increment the value of temp to keep the counter moving
 
-            while (suite.cycles[j] != suite.card && j < suite.temp)
+            // Here checking will be made for numbers which have already been drawn
+            while (suite.cycles[j] != suite.cardNumber && j < suite.temp)
             {
                 j++;
             }
 
-            if (suite.cycles[j] != suite.card)
+            // If not similar numbers are found in cycles[] that this statement will execute
+            if (suite.cycles[j] != suite.cardNumber)
             {
-                suite.cycles[suite.index] = suite.card;
+                suite.cycles[suite.index] = suite.cardNumber;   // the number that was generated is stored in cycles[] here
 
-                suite.index += i;
+                suite.index += i;   // the index of cycles is moved forward by one for the next number
 
-                printf ("You drew a %s \n", cards[suite.card]);
+                printf ("You drew a %s \n", cards[suite.cardNumber]);
                 printf ("Please draw again, press enter \n");
 
                 j = 0;
@@ -108,7 +116,8 @@ int cardDraw ()
                 goto draw;
             }
 
-            else if (suite.cycles[j] == suite.card)
+            // If there is a equal number found another number will be generated
+            else if (suite.cycles[j] == suite.cardNumber)
             {
                 j = 0;
 
@@ -117,9 +126,10 @@ int cardDraw ()
         }
     }
 
+    // If a 11, 24, 37 or 50 is generated this statement is executed
     else 
     {
-        printf ("You drew a %s, YOU WON THE GAME!!! \n", cards[suite.card]);
+        printf ("You drew a %s, YOU WON THE GAME!!! \n", cards[suite.cardNumber]);
 
         return 0;
     }
