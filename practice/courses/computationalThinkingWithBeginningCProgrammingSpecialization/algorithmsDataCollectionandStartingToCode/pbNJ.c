51628144c4dca0ab76b827bcb59ff96a3424d207
Chosen808 <learnkingdom@gmail.com>
2023-11-20 02:06:45 -0400

    Commit 201123 206

--- practice/courses/computationalThinkingWithBeginningCProgrammingSpecialization/algorithmsDataCollectionandStartingToCode/pbNJ.c
+++ practice/courses/computationalThinkingWithBeginningCProgrammingSpecialization/algorithmsDataCollectionandStartingToCode/pbNJ.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
-char array[5][15] = {"Peanut Butter", "Jelly", "Knife", "Bread"};
-
-int tableSet (char a[], char b[], int c)
+typedef struct meal 
 {
-    int temp = -1;
+    char onTable[10][10];
+    int i;
+} meal_t;
+
+char array[6][15] = {"Peanut Butter", "Jelly", "Knife", "Bread", "Plate"};
+
+meal_t choice;
+
+int tableSet (int c, int count)
+{
+    int temp = 0;
 
     if (c == 1)
     {
@@ -27,40 +35,71 @@ int tableSet (char a[], char b[], int c)
         c = 3;
     }
 
-    if (c > -1)
+    else if (c == 5)
     {
-        temp += 1;
-
-        strcpy (a[temp], b[c]);
+        c == 4;
     }
+
+    temp = count - 1;
+
+    printf ("TEMP = %d \n", temp);
+
+    strcpy(choice.onTable[temp], array[c]);
+
+    printf ("array[%d] = %s \n", c, array[c]);
+
+    char *p = array[1];
+    char *o = choice.onTable[0];
+
+    printf ("p = %p \n", p);
+    printf ("o = %p \n", o);
 }
 
 main ()
 {
     char answer[10];
-    char onTable[5][15] = {" "};
     int toGet;
+    int temp = 0;
 
-    printf ("Do you have what you need to make a sandwich? \n");
+    doYou:
+    printf ("Do you have what you need to make a sandwich? Please answer yes or no: \n");
     scanf ("%s", &answer);
 
-    if ((strcmp (answer, "yes")) == 0)
+    if ((strcmp (answer, "yes")) == 0 || (strcmp (answer, "Yes")) == 0)
     {
         printf ("Enjoy your sandwich \n");
+
+        return 0;
     }
 
-    else if ((strcmp (answer, "No")) == 0)
+    else if ((strcmp (answer, "No")) == 0 || (strcmp (answer, "no")) == 0)
     {
-        printf ("Please get what you need \n");
+        for (int i = 0; i < 6; i++)
+        {
+            printf ("Please select what you need: \n");
+            printf ("1. Peanut Butter \n2. Jelly \n3. Knife \n4. Bread \n5. Plate \n");
+            scanf ("%d", &toGet);
+
+            temp += 1;
+
+            tableSet (toGet, temp);
+
+            for (int i = 0; i < temp; i++)
+            {
+                printf ("\n");
+                printf ("You have: %s \n\n", choice.onTable[i]);
+            }
+        }    
     }
 
-    printf ("Please choose what you need: \n");
-    printf ("1. Peanut Butter \n2. Jelly \n3. Knife \n4. Bread \n");
-    scanf ("%d", &toGet);
+    else 
+    {
+        printf ("Please enter yes or no: ");
 
-    tableSet (onTable, array, toGet);
+        goto doYou;
+    }
 
-    printf ("%s \n", onTable[0]);
+    
 
 
     return 0;
