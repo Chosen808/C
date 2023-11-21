964dec12dd903a144f3fc5c7b008b0b604241734
Chosen808 <learnkingdom@gmail.com>
2023-11-20 20:50:47 -0400

    Commit 201123 2050

--- practice/courses/computationalThinkingWithBeginningCProgrammingSpecialization/algorithmsDataCollectionandStartingToCode/pbNJ.c
+++ practice/courses/computationalThinkingWithBeginningCProgrammingSpecialization/algorithmsDataCollectionandStartingToCode/pbNJ.c
@@ -149,7 +149,7 @@ main ()
 
             if (construct == 1)
             {
-                printf ("\nThe jelly butter is uncovered \n");
+                printf ("\nThe jelly is uncovered \n");
 
                 goto breadO;
             }
@@ -166,6 +166,243 @@ main ()
         {
             breadO:
             printf ("Is the bread open \n");
+            scanf ("%s", &answer);
+
+            if ((strcmp (answer, "no")) == 0 || (strcmp (answer, "No")) == 0)
+            {
+                printf ("Open the bread? \n");
+                printf ("Select: \n1. Open \n2. Leave closed \n");
+                scanf ("%d", &construct);
+
+                if (construct == 1)
+                {
+                    printf ("\nThe bread is open \n");
+
+                    goto sliceO;
+                }
+
+                else if (construct == 2)
+                {
+                    printf ("\nPlease open the bread \n");
+
+                    goto breadO;
+                }
+            }
+
+            else if ((strcmp (answer, "Yes")) == 0 || (strcmp (answer, "yes")) == 0)
+            {
+                sliceO:
+                printf ("\nDid you take out two slices of bread? \n");
+                scanf ("%s", &answer);
+
+                if ((strcmp (answer, "no")) == 0 || (strcmp (answer, "No")) == 0)
+                {
+                    chooseB:
+                    printf ("\nTake out two slices of bread?\n");
+                    printf ("Select: \n1. Remove two slices \n2. Do not remove two slice \n");
+                    scanf ("%d", &construct);
+
+                    if (construct == 1)
+                    {
+                        printf ("\nTwo slices are removed");
+
+                        goto plateO;
+                    }
+
+                    else if (construct == 2)
+                    {
+                        printf ("\nPlease remove two slices of bread\n");
+
+                        goto chooseB;
+                    }
+                }
+
+                else if ((strcmp (answer, "Yes")) == 0 || (strcmp (answer, "yes")) == 0)
+                {
+                    plateO:
+                    printf ("\nDid you put the bread on the plate? \n");
+                    scanf ("%s", &answer);
+
+                    if ((strcmp (answer, "no")) == 0 || (strcmp (answer, "No")) == 0)
+                    {
+                        chooseC:
+                        printf ("\nPlace bread on plate? \n");
+                        printf ("Select: \n1. Place bread on plate \n2. Do not place bread on plate \n");
+                        scanf ("%d", &construct);
+
+                        if (construct == 1)
+                        {
+                            printf ("\nBread is on the plate\n");
+
+                            goto knifeO;
+                        }
+
+                        else (construct == 2)
+                        {
+                            printf ("Please place bread on the plate \n");
+
+                            goto chooseC;
+                        }
+                    }
+
+                    else if ((strcmp (answer, "Yes")) == 0 || (strcmp (answer, "yes")) == 0)
+                    {
+                        knifeO:
+                        printf ("\nDid you take the knife? \n");
+                        scanf ("%s", &answer);
+
+                        if ((strcmp (answer, "No")) == 0 || (strcmp (answer, "no")) == 0)
+                        {
+                            chooseD:
+                            printf ("\nTake the knife? \n");
+                            printf ("Select: \n1. Take the knife \n2. Do not take the knife \n");
+                            scanf ("%d", &construct);
+
+                            if (construct == 1)
+                            {
+                                printf ("\nYou have taken the knife \n");
+
+                                goto pbKnifeO;
+                            }
+
+                            else if (construct == 2)
+                            {
+                                printf ("\nPlease take the knife \n");
+
+                                goto chooseD;
+                            }
+                        }
+
+                        else if ((strcmp (answer, "Yes")) == 0 || (strcmp (answer, "yes")) == 0)
+                        {
+                            pbKnifeO:
+                            printf ("\nDid you remove peanut butter with the knife? \n");
+                            scanf ("%s", &answer);
+
+                            if ((strcmp (answer, "no")) == 0 || (strcmp (answer, "No")) == 0)
+                            {
+                                chooseE:
+                                printf ("\nRemove peanut butter with the knife? \n");
+                                printf ("Select: \n1. Remove peanut butter with the knife \n2. Do not remove peanut butter \n");
+                                scanf ("%d", %construct);
+
+                                if (construct == 1)
+                                {
+                                    printf ("\nYou have remove peanut butter with the knife \n");
+
+                                    goto spreadPbO;
+                                }
+
+                                else if (construct == 2)
+                                {
+                                    printf ("\nPlease remove preanut butter with the knife \n");
+
+                                    goto chooseE;
+                                }
+                            }
+
+                            else if ((strcmp (answer, "Yes")) == 0 || (strcmp (answer, "yes")) == 0)
+                            {
+                                spreadPbO:
+                                printf ("\nDid you spread the peanut butter on the knife on one slice of bread? \n");
+                                scanf ("%s", &answer);
+
+                                if ((strcmp (answer, "No")) == 0 || (strcmp (answer, "no")) == 0)
+                                {
+                                    chooseF:
+                                    printf ("\nSpread peanut butter on one slice of bread? \n");
+                                    printf ("Select: \n1. Spread peanut butter on one slice of bread \n2. Do not spread peanut butter \n");
+                                    scanf ("%d", &construct);
+
+                                    if (construct == 1)
+                                    {
+                                        printf ("\nYou have spread the peanut butter on one slice of bread \n");
+
+                                        goto jKnifeO;
+                                    }
+
+                                    else if (construct == 2)
+                                    {
+                                        printf ("\nPlease spread peanut butter on one slice of bread \n");
+                                         goto chooseF;
+                                    }
+                                }
+
+                                else if ((strcmp (answer, "Yes")) == 0 || (strcmp (answer, "yes")) == 0)
+                                {
+                                    jKnifeO:
+                                    printf ("\nDid you remove jelly with the knife? \n");
+                                    scanf ("%s", &answer);
+
+                                    if ((strcmp (answer, "No")) == 0 || (strcmp (answer, "no")) == 0)
+                                    {
+                                        chooseG:
+                                        printf ("\nRemove jelly with the knife? \n");
+                                        printf ("Select: \n1. Remove with the knife \n2. Do not remove jelly with knife \n");
+                                        scanf ("%d", &construct);
+
+                                        if (construct == 1)
+                                        {
+                                            printf ("\nYou have removed jelly with the knife\n");
+
+                                            goto spreadJO;
+                                        }
+
+                                        else if (construct == 2)
+                                        {
+                                            printf ("\nPlease remove jelly with the knife \n");
+
+                                            goto chooseG;
+                                        }
+                                    }
+
+                                    else if ((strcmp (answer, "Yes")) == 0 || (strcmp (answer, "yes")) == 0)
+                                    {
+                                        spreadJO:
+                                        printf ("\nDid you spread the jelly on the other slice of bread? \n");
+                                        scanf ("%s", &answer);
+
+                                        if ((strcmp (answer, "no")) == 0 || (strcmp (answer, "No")) == 0)
+                                        {
+                                            chooseH:
+                                            printf ("\nSpread jelly on the other slice of bread? \n");
+                                            printf ("Select: \n1. Spread the jelly on other slice of bread \n2. Do not spread jelly on other slice of bread\n");
+                                            scanf ("%d", &construct);
+
+                                            if (construct == 1)
+                                            {
+                                                printf ("\nYou have spread jelly on the other slice of bread \n");
+
+                                                goto knifeDownO;
+                                            }
+
+                                            else if (construct == 2)
+                                            {
+                                                printf ("\nPlease spread jelly on the other slice of bread \n");
+
+                                                goto chooseH;
+                                            }
+                                        }
+
+                                        else if ((strcmp (answer, "Yes")) == 0 || (strcmp (answer, "yes")) == 0)
+                                        {
+                                            knifeDownO:
+                                            printf ("\nDid you put down the knife? \n");
+                                            scanf ("%s", &answer);
+
+                                            if ((strcmp (answer, "no")) == 0 || (strcmp (answer, "No")) == 0)
+                                            {
+                                                chooseI:
+                                                printf ("\nPlea")
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
     return 0;
