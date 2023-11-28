a49e7e68703c4bab0c1c321496a4661c900093cc
Chosen808 <learnkingdom@gmail.com>
2023-11-28 15:22:42 -0400

    Commit 281123 1522

--- practice/courses/computationalThinkingWithBeginningCProgrammingSpecialization/algorithmsDataCollectionandStartingToCode/wk2/floatCal.c
+++ practice/courses/computationalThinkingWithBeginningCProgrammingSpecialization/algorithmsDataCollectionandStartingToCode/wk2/floatCal.c
@@ -6,17 +6,21 @@
 
 main ()
 {
+    // Problem 1: Convert between temperatures
     float fahrenheit;
     float celcius;
     float calFahrenheit;
 
+    // Prompt for and get original Fahrenheit temperature
     printf ("Please enter your temperature in Fahrenheit: ");
     scanf ("%f", &fahrenheit);
 
+    // Calculate and display temperature in Celcius
     celcius = (fahrenheit -32) / 9 * 5;
 
     printf ("Your temperature in Celcius is: %.2f\n", celcius);
 
+    // Calculate and display temperature in Fahrenheit
     calFahrenheit = celcius * 9 / 5 + 32;
 
     printf ("You temperature in Fahrenheit is: %.2f \n", fahrenheit);
