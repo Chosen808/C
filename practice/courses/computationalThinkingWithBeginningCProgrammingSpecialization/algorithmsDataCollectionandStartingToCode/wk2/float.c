b166a51e0691ff4b6cb0b3720de59b1cbc3f631c
Chosen808 <learnkingdom@gmail.com>
2023-11-28 14:38:27 -0400

    Commit 281123 1438

--- practice/courses/computationalThinkingWithBeginningCProgrammingSpecialization/algorithmsDataCollectionandStartingToCode/wk2/float.c
+++ practice/courses/computationalThinkingWithBeginningCProgrammingSpecialization/algorithmsDataCollectionandStartingToCode/wk2/float.c
@@ -6,13 +6,18 @@
 
 main ()
 {
-    int score = 64;
+    // Problem 1: Save and display score
+    int score = 6359;
 
     printf ("Score = %d \n", score);
 
-    float percent = (float) MAX_SCORE / score;
+    // Problem 2: Calculate and display percentage
+    float percent = ((float) MAX_SCORE / score) * 100;
 
-    printf ("Percent = %.0f \n", percent);
+    printf ("Percent = %.2f \n", percent);
+
+    // Problem 3: Display rounded percentage
+    printf ("Rounded Percentage = %.0f \n", percent);
 
     return 0;
 }
