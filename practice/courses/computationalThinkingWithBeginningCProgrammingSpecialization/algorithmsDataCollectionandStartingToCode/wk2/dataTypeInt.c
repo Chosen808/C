7da9c2b52b450c329f3fc96e0ab1d5946ebbcb64
Chosen808 <learnkingdom@gmail.com>
2023-11-28 12:08:12 -0400

    Commit 281123 1208

--- practice/courses/computationalThinkingWithBeginningCProgrammingSpecialization/algorithmsDataCollectionandStartingToCode/wk2/dataTypeInt.c
+++ practice/courses/computationalThinkingWithBeginningCProgrammingSpecialization/algorithmsDataCollectionandStartingToCode/wk2/dataTypeInt.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define YEAR_TO_DATE 2023
+
 
 main ()
 {
     int age = 37;
 
     printf ("Age = %d \n", age);
+
+    int oldAge = YEAR_TO_DATE - 1962;
+
+    printf ("Old Age = %d \n", oldAge);
 
     return 0;
 }
