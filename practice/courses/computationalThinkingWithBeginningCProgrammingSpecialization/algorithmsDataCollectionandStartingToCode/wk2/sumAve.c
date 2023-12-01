e72c47b39848a3cc6460fcd46ecc1f72cc3cfbb7
Chosen808 <learnkingdom@gmail.com>
2023-11-30 21:10:13 -0400

    Commit 301123 2110

--- practice/courses/computationalThinkingWithBeginningCProgrammingSpecialization/algorithmsDataCollectionandStartingToCode/wk2/sumAve.c
+++ practice/courses/computationalThinkingWithBeginningCProgrammingSpecialization/algorithmsDataCollectionandStartingToCode/wk2/sumAve.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include 
 
 #define MAX_LENGTH 100
 
---
+++ practice/courses/computationalThinkingWithBeginningCProgrammingSpecialization/algorithmsDataCollectionandStartingToCode/wk2/tempCal.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+main ()
+{
+    int fahrenheit = 0;
+    int celsius = (fahrenheit - 32) / 9 * 5;
+    int calFahrenheit = celsius * 9 / 5 + 32;
+
+    printf ("Temp = %d \n", calFahrenheit);
+
+   printf("s= %f \n", sqrt (16));
+
+    return 0;
+}
